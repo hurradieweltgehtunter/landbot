ecb5cc16d57dbf27779bc1dd45d73261955e7f38
hurradieweltgehtunter <florian@owncloud.com>
2019-06-03 10:32:24 +0200

    show proactive message only on first load

--- js/landbot.js
+++ js/landbot.js
@@ -1,10 +1,4 @@
-window.onload = function() {
-
-}();
-
-
 $(document).ready(function() {
-
   $.ajax({
     url: 'https://static.landbot.io/landbot-widget/landbot-widget-1.0.0.js',
     dataType: "script",
@@ -15,21 +9,24 @@ $(document).ready(function() {
 
       var urlCustomAvatar = "https://storage.googleapis.com/media.helloumi.com/channels/IRSRASHJB13UYVU6DCOWZBJCJUUTRRD7.jpg";
 
-      //DO NOT EDIT BELOW THIS LINE//
       var holderUrl = `url(${urlCustomAvatar})`
       myLandbotLivechat.on('landbot-load', () => {
         window.document.getElementsByClassName("LandbotProactive__frame__content")[0].contentDocument.body.childNodes[0].childNodes[0].childNodes[3].childNodes[1].childNodes[0].childNodes[0].childNodes[0].style.backgroundImage = holderUrl
 
         var frameBody = window.document.getElementsByClassName("LandbotProactive__frame__content")[0].contentDocument.body;
         frameBody.querySelector(".hu-message-author").innerHTML = "ownCloud.online"
-      })
+      });
 
-      setTimeout(() => {
-        myLandbotLivechat.sendProactive("Hello there!");
-      }, 5000);
+      myLandbotLivechat.on('landbot-msg-receive', (data) => {
+        localStorage.setItem('proactiveShown', 'true');
+      });
+
+      var proactiveShown = localStorage.getItem('proactiveShown');
+      if (proactiveShown != 'true'){
+        setTimeout(() => {
+          myLandbotLivechat.sendProactive("Hello there!");
+        }, 5000);
+      }
     }
   });
-
-
-
 })
